f4b648c0f4e90e776d4a26c577aef3642e809c4e
Sean Salamone <smsalamone513@gmail.com>
2019-10-07 20:15:50 -0400

    night seven updates

--- public/main.js
+++ public/main.js
@@ -1,7 +1,12 @@
+
 const main = () => {
   if (document.querySelector('h1.hello-world')) {
     document.querySelector('h1.hello-world').textContent = 'Hello, World!'
   }
+  if (document.querySelector('h2')){
+    document.querySelector('h2').style.color = 'blue'
+  }
+  document.querySelector('body').style.backgroundColor = 'gray'
 }
 
 document.addEventListener('DOMContentLoaded', main)
